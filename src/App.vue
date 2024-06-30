02e23080b1be8187f44a4689e1c769c168cc69ef
Misam-Lakhotara <misamlakhotara@gmail.com>
2024-06-30 14:46:12 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted } from 'vue';
+const FetchData = async () => {
+  const response = await fetch('http://localhost:8080/api/colors');
+  const data = await response.json();
+  console.log(data);
+};
+
+onMounted(() => {
+  FetchData();
+});
+
+
+
+</script>
+
+<template>
+  <div>
+    11
+  </div>
+</template>
